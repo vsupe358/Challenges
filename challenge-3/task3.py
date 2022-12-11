22f98e48921431b20d126a0792475d31d307076e
Vaibhav Supe <vaibhavsupe@gmail.com>
2022-12-11 23:56:19 +0530

    Add files via upload
    added 3rd task

---
+++ challenge-3/task3.py
@@ -0,0 +1,26 @@
+def getKey(obj: dict):
+    keys = list(obj)
+    if len(keys) != 1:
+        raise Exception('either multiple keys or empty dict found')
+    else:
+        return keys[0]
+
+
+def getNestedValue(obj: dict, key: str, isFound = False):
+    # print(obj, key, isFound)
+    if type(obj) is not dict and not isFound:
+        return None
+    if (isFound or (key in obj.keys())) :
+        if type(obj[key]) is dict:
+            return getNestedValue(obj[key], getKey(obj[key]), True)
+        else:
+            # print(f'obj[getKey(obj)]: {obj[getKey(obj)]}')
+            return obj[getKey(obj)]
+    else:
+        nestedKey = getKey(obj)
+        return getNestedValue(obj[nestedKey], key, False)
+
+if __name__ == '__main__':
+    obj = {'a': {'b': {'c': 'd'}}}
+    value = getNestedValue(obj, 'c')
+    print(value)
